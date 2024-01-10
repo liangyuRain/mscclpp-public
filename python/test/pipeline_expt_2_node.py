79ae1cd2fd10825a5598d7b8ba106e7c4ce358ab
liangyuRain <liangyu@cs.washington.edu>
2024-01-10 00:31:25 +0000

    peer to peer bw test

--- python/test/pipeline_expt_2_node.py
+++ python/test/pipeline_expt_2_node.py
@@ -212,6 +212,72 @@ def multi_instance(Ts: dict, Cs: dict, k: int, ninstance: int):
     return rTs, rCs, k * ninstance
 
 
+def test_peer_to_peer(group: mscclpp_comm.CommGroup, connections: dict, src: int, dest: int):
+    recv_sm_channels, send_sm_channels = {}, {}
+    recv_proxy_channels, send_proxy_channels = {}, {}
+    nelem_total = 2 ** 28
+    nelem_per_send = 2 ** 18
+    nblocks = 24
+    assert nelem_total % nblocks == 0
+    shard_size = nelem_total // nblocks
+    proxy_service = ProxyService()
+    if group.my_rank == src:
+        # sender
+        memory = cp.arange(nelem_total, dtype=cp.int32)
+        if group.my_rank // 8 == dest // 8:
+            send_sm_channels = {bid: [group.make_sm_channel(memory, connections[dest], dest)]
+                                for bid in range(nblocks)}
+        else:
+            send_proxy_channels = {bid: [group.make_proxy_channel(proxy_service, memory, connections[dest], dest)]
+                                   for bid in range(nblocks)}
+    elif group.my_rank == dest:
+        # recver
+        memory = cp.zeros(nelem_total, dtype=cp.int32)
+        if group.my_rank // 8 == src // 8:
+            recv_sm_channels = {bid: [group.make_sm_channel(memory, connections[src], src)]
+                                for bid in range(nblocks)}
+        else:
+            recv_proxy_channels = {bid: [group.make_proxy_channel(proxy_service, memory, connections[src], src)]
+                                   for bid in range(nblocks)}
+    else:
+        pass
+
+    data_offsets = {bid: shard_size * bid for bid in range(nblocks)}
+    data_sizes = {bid: shard_size for bid in range(nblocks)}
+    scratch_size = 0
+    node_types = {bid: 1 for bid in range(nblocks)}
+    
+    if group.my_rank == src or group.my_rank == dest:
+        kernel = PipelineKernel(recv_sm_channels, send_sm_channels, recv_proxy_channels, send_proxy_channels,
+                                memory, data_offsets, data_sizes, scratch_size, {}, {}, node_types, 
+                                nelem_per_send, nblocks)
+    else:
+        kernel = None
+
+    proxy_service.start_proxy()
+    group.barrier()
+    if kernel is not None:
+        res = benchmark(lambda: kernel(), n_warmup=100, n_repeat=100).gpu_times
+    else:
+        res = [-1]
+    avg_time = np.average(res)  # seconds
+    min_time = np.min(res)
+
+    group.barrier()
+    proxy_service.stop_proxy()
+
+    size = nelem_total * 4
+    if group.my_rank == src:
+        print_row(str((src, dest)),
+                size, 
+                f"{avg_time * 1e6:.2f}",
+                f"{min_time * 1e6:.2f}",
+                f"{(size / 2 ** 30) / avg_time:.2f}",
+                f"{(size / 2 ** 30) / min_time:.2f}")
+    if kernel is not None:
+        assert cp.array_equal(memory, cp.arange(nelem_total, dtype=cp.int32))
+
+
 if __name__ == "__main__":
     if MPI.COMM_WORLD.rank < 8:
         os.environ["MSCCLPP_HCA_DEVICES"] = ",".join([f"mlx5_{i}" for i in range(9) if i != 3])
