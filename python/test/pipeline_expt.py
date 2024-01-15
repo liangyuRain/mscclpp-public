980d389f883d2156d364ee24a99ee033144eabe0
liangyuRain <liangyu@cs.washington.edu>
2024-01-15 07:00:50 +0000

    reduce-scatter + allgather expt

--- python/test/pipeline_expt.py
+++ python/test/pipeline_expt.py
@@ -48,12 +48,11 @@ def print_row(*args):
     print("".join(f"{arg:>20}" for arg in args), flush=True)
 
 
-def run_expt(group: mscclpp_comm.CommGroup, kernel: PipelineKernel,
+def run_expt(group: mscclpp_comm.CommGroup, func,
              init_data: cp.array, data: cp.array,
-             length: int, nelem_per_send: int, k: int,
+             length: int, nelem_per_send: int,
              correctness_check,
              check_iters: int, warmup_iters: int, iters: int):
-    func = kernel.get_func(nelem_total=length, nelem_per_send=nelem_per_send)
     group.barrier()
     for _ in range(check_iters):
         cp.copyto(data[:length], init_data)
@@ -107,7 +106,8 @@ def run_allreduce(Ts: dict, Cs: dict, k: int, group: mscclpp_comm.CommGroup,
         print(f"nranks={group.nranks}")
         print(f"k={k}, scratch_size={scratch_size}")
         print(f"check_iters={check_iters}, warmup_iters={warmup_iters}, iters={iters}")
-        print(f"KERNEL={kernel.kernel_file}::{kernel.kernel_name}, BENCH_METHOD={BENCH_METHOD}")
+        print(f"KERNEL={kernel.kernel_file}::{kernel.kernel_name}")
+        print(f"BENCH_METHOD={BENCH_METHOD}")
         print()
         print_row("size(B)", "send_size(B)", "avg_time(us)", "min_time(us)", "avg_algbw(GB/s)", "max_algbw(GB/s)")
 
@@ -124,8 +124,75 @@ def run_allreduce(Ts: dict, Cs: dict, k: int, group: mscclpp_comm.CommGroup,
         else:
             init_data, correctness_check = None, None
 
-        run_expt(group=group, kernel=kernel, init_data=init_data, data=data,
-                 length=length, nelem_per_send=nelem_per_send, k=k,
+        func = kernel.get_func(nelem_total=length, nelem_per_send=nelem_per_send)
+        run_expt(group=group, func=func, init_data=init_data, data=data,
+                 length=length, nelem_per_send=nelem_per_send,
+                 correctness_check=correctness_check,
+                 check_iters=check_iters, warmup_iters=warmup_iters, iters=iters)
+
+    proxy_service.stop_proxy()
+
+
+def run_fusion_allreduce(reduce_scatter_Ts: dict, reduce_scatter_Cs: dict, reduce_scatter_k: int,
+                         allgather_Ts: dict, allgather_Cs: dict, allgather_k: int,
+                         group: mscclpp_comm.CommGroup,
+                         connections: dict, connection_types: dict,
+                         data_lengths: list, send_lengths: list, scratch_size: int,
+                         check_iters: int = 10, warmup_iters: int = 10, iters: int = 10):
+    proxy_service = ProxyService()
+
+    lcm = reduce_scatter_k * allgather_k // math.gcd(reduce_scatter_k, allgather_k)
+    alignment = 4 * lcm * group.nranks
+    max_length = max(math.ceil(length / alignment) * alignment for length in data_lengths)
+    data = cp.empty(max_length, dtype=cp.int32)
+    RS_kernel = reduce_scatter_kernel(reduce_scatter_Ts, reduce_scatter_Cs, reduce_scatter_k,
+                                      group=group,
+                                      connections=connections,
+                                      connection_types=connection_types,
+                                      data=data,
+                                      scratch_size=scratch_size,
+                                      proxy_service=proxy_service,
+                                      use_reduceScatter_kernel=True)
+    AG_kernel = allgather_kernel(allgather_Ts, allgather_Cs, allgather_k,
+                                 group=group,
+                                 connections=connections,
+                                 connection_types=connection_types,
+                                 data=data,
+                                 proxy_service=proxy_service)
+
+    if group.my_rank == 0:
+        print("#" * 55 + " Allreduce " + "#" * 55)
+        print(f"nranks={group.nranks}")
+        print(f"RS_k={reduce_scatter_k}, AG_k={allgather_k}, scratch_size={scratch_size}")
+        print(f"check_iters={check_iters}, warmup_iters={warmup_iters}, iters={iters}")
+        print(f"RS_KERNEL={RS_kernel.kernel_file}::{RS_kernel.kernel_name}")
+        print(f"AG_KERNEL={AG_kernel.kernel_file}::{AG_kernel.kernel_name}")
+        print(f"BENCH_METHOD={BENCH_METHOD}")
+        print()
+        print_row("size(B)", "send_size(B)", "avg_time(us)", "min_time(us)", "avg_algbw(GB/s)", "max_algbw(GB/s)")
+
+    proxy_service.start_proxy()
+
+    for length, nelem_per_send in itertools.product(data_lengths, send_lengths):
+        if length % alignment != 0:
+            length = math.ceil(length / alignment) * alignment
+
+        if check_iters > 0:
+            init_data = cp.array([group.my_rank + 1] * length, dtype=cp.int32)
+            expected = cp.array([sum(n + 1 for n in range(group.nranks))] * length, dtype=cp.int32)
+            correctness_check = lambda: cp.array_equal(data[:length], expected)
+        else:
+            init_data, correctness_check = None, None
+
+        RS_func = RS_kernel.get_func(nelem_total=length, nelem_per_send=nelem_per_send)
+        AG_func = AG_kernel.get_func(nelem_total=length, nelem_per_send=nelem_per_send)
+
+        def func(stream_ptr=None):
+            RS_func(stream_ptr)
+            AG_func(stream_ptr)
+
+        run_expt(group=group, func=func, init_data=init_data, data=data,
+                 length=length, nelem_per_send=nelem_per_send,
                  correctness_check=correctness_check,
                  check_iters=check_iters, warmup_iters=warmup_iters, iters=iters)
 
@@ -153,7 +220,8 @@ def run_allgather(Ts: dict, Cs: dict, k: int, group: mscclpp_comm.CommGroup,
         print(f"nranks={group.nranks}")
         print(f"k={k}")
         print(f"check_iters={check_iters}, warmup_iters={warmup_iters}, iters={iters}")
-        print(f"KERNEL={kernel.kernel_file}::{kernel.kernel_name}, BENCH_METHOD={BENCH_METHOD}")
+        print(f"KERNEL={kernel.kernel_file}::{kernel.kernel_name}")
+        print(f"BENCH_METHOD={BENCH_METHOD}")
         print()
         print_row("size(B)", "send_size(B)", "avg_time(us)", "min_time(us)", "avg_algbw(GB/s)", "max_algbw(GB/s)")
 
@@ -171,8 +239,9 @@ def run_allgather(Ts: dict, Cs: dict, k: int, group: mscclpp_comm.CommGroup,
         else:
             init_data, correctness_check = None, None
 
-        run_expt(group=group, kernel=kernel, init_data=init_data, data=data,
-                 length=length, nelem_per_send=nelem_per_send, k=k,
+        func = kernel.get_func(nelem_total=length, nelem_per_send=nelem_per_send)
+        run_expt(group=group, func=func, init_data=init_data, data=data,
+                 length=length, nelem_per_send=nelem_per_send,
                  correctness_check=correctness_check,
                  check_iters=check_iters, warmup_iters=warmup_iters, iters=iters)
 
@@ -203,7 +272,8 @@ def run_reduce_scatter(Ts: dict, Cs: dict, k: int, group: mscclpp_comm.CommGroup
         print(f"nranks={group.nranks}")
         print(f"k={k}, scratch_size={scratch_size}")
         print(f"check_iters={check_iters}, warmup_iters={warmup_iters}, iters={iters}")
-        print(f"KERNEL={kernel.kernel_file}::{kernel.kernel_name}, BENCH_METHOD={BENCH_METHOD}")
+        print(f"KERNEL={kernel.kernel_file}::{kernel.kernel_name}")
+        print(f"BENCH_METHOD={BENCH_METHOD}")
         print()
         print_row("size(B)", "send_size(B)", "avg_time(us)", "min_time(us)", "avg_algbw(GB/s)", "max_algbw(GB/s)")
 
@@ -225,8 +295,9 @@ def run_reduce_scatter(Ts: dict, Cs: dict, k: int, group: mscclpp_comm.CommGroup
         else:
             init_data, correctness_check = None, None
 
-        run_expt(group=group, kernel=kernel, init_data=init_data, data=data,
-                 length=length, nelem_per_send=nelem_per_send, k=k,
+        func = kernel.get_func(nelem_total=length, nelem_per_send=nelem_per_send)
+        run_expt(group=group, func=func, init_data=init_data, data=data,
+                 length=length, nelem_per_send=nelem_per_send,
                  correctness_check=correctness_check,
                  check_iters=check_iters, warmup_iters=warmup_iters, iters=iters)
 
@@ -250,14 +321,14 @@ if __name__ == "__main__":
     bench_iters = 50
 
     # allpairs
-    k = 4
-    Ts = {(u, i): [[(u, v)] for v in range(group.nranks) if u != v]
-          for u, i in itertools.product(range(group.nranks), range(k))}
-    Cs = {(u, i): 1 for u, i in itertools.product(range(group.nranks), range(k))}
+    AG_k = 4
+    AG_Ts = {(u, i): [[(u, v)] for v in range(group.nranks) if u != v]
+             for u, i in itertools.product(range(group.nranks), range(AG_k))}
+    AG_Cs = {(u, i): 1 for u, i in itertools.product(range(group.nranks), range(AG_k))}
     connections = connect_nvlink(group, [v for v in range(group.nranks) 
                                          if v != group.my_rank])
 
-    run_allgather(Ts, Cs, k, group=group, connections=connections, 
+    run_allgather(AG_Ts, AG_Cs, AG_k, group=group, connections=connections, 
                   connection_types={dest: channel_type(dest) for dest in connections},
                   data_lengths=data_lengths,
                   send_lengths=[2 ** 18],
@@ -269,15 +340,15 @@ if __name__ == "__main__":
         print()
 
     # ring
-    k = 8
-    Ts = {(u, i): [[((u + d) % group.nranks, (u + d + 1) % group.nranks)]
-                   for d in range(group.nranks - 1)]
-          for u, i in itertools.product(range(group.nranks), range(k))}
-    Cs = {(u, i): 1 for u, i in itertools.product(range(group.nranks), range(k))}
+    RS_k = 8
+    RS_Ts = {(u, i): [[((u + d) % group.nranks, (u + d + 1) % group.nranks)]
+                      for d in range(group.nranks - 1)]
+             for u, i in itertools.product(range(group.nranks), range(RS_k))}
+    RS_Cs = {(u, i): 1 for u, i in itertools.product(range(group.nranks), range(RS_k))}
     connections = connect_nvlink(group, [(group.my_rank - 1) % group.nranks,
                                          (group.my_rank + 1) % group.nranks])
 
-    run_reduce_scatter(Ts, Cs, k, group=group, connections=connections, 
+    run_reduce_scatter(RS_Ts, RS_Cs, RS_k, group=group, connections=connections, 
                        connection_types={dest: channel_type(dest) for dest in connections},
                        data_lengths=data_lengths,
                        send_lengths=[2 ** 18],
@@ -291,15 +362,15 @@ if __name__ == "__main__":
         print()
 
     # ring
-    k = 4
-    Ts = {(u, i): [[((u + d) % group.nranks, (u + d + 1) % group.nranks)]
-                   for d in range(group.nranks - 1)]
-          for u, i in itertools.product(range(group.nranks), range(k))}
-    Cs = {(u, i): 1 for u, i in itertools.product(range(group.nranks), range(k))}
+    AR_k = 4
+    AR_Ts = {(u, i): [[((u + d) % group.nranks, (u + d + 1) % group.nranks)]
+                      for d in range(group.nranks - 1)]
+             for u, i in itertools.product(range(group.nranks), range(AR_k))}
+    AR_Cs = {(u, i): 1 for u, i in itertools.product(range(group.nranks), range(AR_k))}
     connections = connect_nvlink(group, [(group.my_rank - 1) % group.nranks,
                                          (group.my_rank + 1) % group.nranks])
 
-    run_allreduce(Ts, Cs, k, group=group, connections=connections, 
+    run_allreduce(AR_Ts, AR_Cs, AR_k, group=group, connections=connections, 
                   connection_types={dest: channel_type(dest) for dest in connections},
                   data_lengths=data_lengths,
                   send_lengths=[2 ** 15],
@@ -307,5 +378,20 @@ if __name__ == "__main__":
                   check_iters=check_iters,
                   warmup_iters=warmup_iters,
                   iters=bench_iters)
+
+    if group.my_rank == 0:
+        print()
+
+    # reduce-scatter + allgather
+    connections = connect_nvlink(group, [r for r in range(group.nranks) if r != group.my_rank])
+    run_fusion_allreduce(RS_Ts, RS_Cs, RS_k, AG_Ts, AG_Cs, AG_k,
+                         group=group, connections=connections, 
+                         connection_types={dest: channel_type(dest) for dest in connections},
+                         data_lengths=data_lengths,
+                         send_lengths=[2 ** 18],
+                         scratch_size=2 ** 20,
+                         check_iters=check_iters,
+                         warmup_iters=warmup_iters,
+                         iters=bench_iters)
 
     del group
