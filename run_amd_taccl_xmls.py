fa6d3f895453fd9247c6c0414a743fe0420c70a7
liangyuRain <liangyu@cs.washington.edu>
2024-01-26 08:22:45 +0000

    amd taccl results

--- run_amd_taccl_xmls.py
+++ run_amd_taccl_xmls.py
@@ -23,20 +23,23 @@ if __name__ == "__main__":
             xml_file = os.path.join(folder, fname)
             os.system(f"cp {xml_file} /home/amdautomation/liangyu/rccl_run_schedule/")
             os.system(f"ssh 10.8 cp {xml_file} /home/amdautomation/liangyu/rccl_run_schedule/")
-            end_size = "10G" if buff_size >= 2 ** 20 in fname else "1G"
+            end_size = "10G" if buff_size >= 2 ** 20 else "1G"
             cmd = (
 f"""mpirun --allow-run-as-root \
 -hostfile ~/hostfile -map-by ppr:16:node \
 --bind-to numa -mca pml ob1 -mca btl ^openib -mca btl_tcp_if_include eth0 \
 -x PATH -x LD_LIBRARY_PATH=/opt/rocm/lib:$LD_LIBRARY_PATH -x NCCL_SOCKET_IFNAME=eth0 \
+-x LD_PRELOAD=/home/amdautomation/liangyu/rccl/build/librccl.so:$LD_PRELOAD \
 -x NCCL_DEBUG=WARN -x NCCL_DEBUG_SUBSYS=INIT,GRAPH -x HSA_FORCE_FINE_GRAIN_PCIE=1 \
 -x NCCL_MIN_NCHANNELS=32 -x NCCL_IB_PCI_RELAXED_ORDERING=1 \
 -x NCCL_NET_GDR_LEVEL=3 -x CUDA_DEVICE_ORDER=PCI_BUS_ID -x NCCL_IBEXT_DISABLE=1 \
 -x NCCL_PROTO=Simple \
 -x NCCL_BUFFSIZE={buff_size} \
+-x RCCL_MSCCL_FORCE_ENABLE=1 \
 -x MSCCL_ALGO_DIR=/home/amdautomation/liangyu/rccl_run_schedule \
 /home/amdautomation/liangyu/rccl-tests/build/{exe} -b 256 -e {end_size} -f 2 -g 1 -z 0 -n 50 -w 50 -c 1 -a 2"""
             )
+            print(cmd)
             res_file = os.path.join(folder, "amd_taccl_results", fname + f"buff{buff_size}.txt")
             os.system(f"stdbuf --output=L {cmd} 2>&1 | tee {res_file}")
             os.system(rm_cmd)
